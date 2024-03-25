f5fa35d511a85f424c601732071ea0b9551d29e2
Sebastian <soutosebastianma@gmail.com>
2024-03-25 10:16:53 -0300

    resolucion taller 2

--- taller2/src/main/java/aed/Debugging.java
+++ taller2/src/main/java/aed/Debugging.java
@@ -9,17 +9,22 @@ class Debugging {
         boolean res = true;
 
         for (int i = 0; i < xs.length; i++) {
-            if (xs[i] != ys[i]) {
+            if (xs.length != ys.length) {
+                res = false;
+            } else if (xs[i] != ys[i]) {
                 res = false;
             }
+
         }
         return res;
     }
 
     boolean ordenado(int[] xs) {
         boolean res = true;
-        for (int i = 0; i < xs.length; i++) {
-            if (xs[i] > xs [i+1]) {
+        for (int i = 0; i < xs.length -1; i++) {
+            if (xs.length == 1) {
+                res = true;
+            } else if (xs[i] > xs [i+1]) {
                 res = false;
             }
         }
@@ -27,22 +32,22 @@ class Debugging {
     }
 
     int maximo(int[] xs) {
-        int res = 0;
-        for (int i = 0; i <= xs.length; i++) {
-            if (xs[i] > res) res = i;
+        int res = xs[0];
+        for (int i = 0; i < xs.length; i++) {
+            if (xs[i] > res) res = xs[i];
         }
         return res;
     }
 
     boolean todosPositivos(int[] xs) {
-        boolean res = false;
+        boolean res = true;
         for (int x : xs) {
-            if (x > 0) {
-                res = true;
-            } else {
+            if (x <= 0) {
                 res = false;
             }
         }
         return res;
+        
     }
+
 }
--- taller2/src/test/java/aed/CoberturaTests.java
+++ taller2/src/test/java/aed/CoberturaTests.java
@@ -11,16 +11,30 @@ class CoberturaTests {
 
     @Test
     void testFizzBuzz() {
-        assertEquals("A","B");
+        assertEquals("FizzBuzz",cobertura.fizzBuzz(15));
+        assertEquals("Fizz",cobertura.fizzBuzz(3));
+        assertEquals("Buzz",cobertura.fizzBuzz(5));
+        assertEquals("2",cobertura.fizzBuzz(2));
     }
 
     @Test
     void testNumeroCombinatorio() {
-        assertTrue(false);
+        assertEquals(1,cobertura.numeroCombinatorio(1,0));
+        assertEquals(1,cobertura.numeroCombinatorio(3,3));
+        assertEquals(0,cobertura.numeroCombinatorio(1,3));
+        assertEquals(3,cobertura.numeroCombinatorio(3,2));
     }
 
     @Test
     void testRepeticionesConsecutivas() {
-        assertTrue(false);
+        assertEquals(0,cobertura.repeticionesConsecutivas(new int[]{1}));
+        assertEquals(1,cobertura.repeticionesConsecutivas(new int[]{1,2,3}));
+        assertEquals(3,cobertura.repeticionesConsecutivas(new int[]{1,1,1,2,3}));
+        assertEquals(3,cobertura.repeticionesConsecutivas(new int[]{1,2,3,3,3,2,5}));
+        assertEquals(3,cobertura.repeticionesConsecutivas(new int[]{1,2,3,4,5,5,5}));
+        assertEquals(3,cobertura.repeticionesConsecutivas(new int[]{1,2,2,3,5,5,5}));
+        assertEquals(4,cobertura.repeticionesConsecutivas(new int[]{1,2,2,2,2,3,4,4}));
+        assertEquals(3,cobertura.repeticionesConsecutivas(new int[]{1,2,2,5,5,5}));
+        assertEquals(4,cobertura.repeticionesConsecutivas(new int[]{1,2,2,2,2,5,5,5}));
     }
 }
