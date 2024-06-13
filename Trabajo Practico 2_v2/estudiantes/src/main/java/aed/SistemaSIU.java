3c78048405d297ad223008f60128545f62facded
Sebastian <soutosebastianma@gmail.com>
2024-06-13 17:17:37 -0300

    grandes cambios

--- Trabajo Practico 2_v2/estudiantes/src/main/java/aed/SistemaSIU.java
+++ Trabajo Practico 2_v2/estudiantes/src/main/java/aed/SistemaSIU.java
@@ -2,6 +2,18 @@ package aed;
 
 public class SistemaSIU {
 
+    //private trie<trie<tuple<String[],Integer[]>>> sistema = new trie<trie<tuple<String[],Integer[]>>>(); //un trie q conecta a tries q conectan a tuplas <alumnos[],numero de docentes[]>
+                                                                                                         //ver estructura esbozada por augustus. 
+
+    //private tuple<String[],Integer[]> datosPorMateria = new tuple<String[],Integer[]>(new String[2],new Integer[2]);    //MUY DUDOSO, EN VEZ DE STRING[] USAR LISTAS ENLAZADAS EN MI OPINION.
+
+    private Trie<Trie<Tupla<ListaEnlazada<String>,ListaEnlazada<Integer>>>> sistema = new Trie<Trie<Tupla<ListaEnlazada<String>,ListaEnlazada<Integer>>>>();
+
+    //private Tupla<ListaEnlazada<String>,ListaEnlazada<Integer>> datosPorMateria = new Tupla<ListaEnlazada<String>, ListaEnlazada<Integer>>(); //me parece q esto es mas un template, va 
+                                                                                                                                              //a cambiar por materia asi q no usar esta
+                                                                                                                                              //misma variable.
+   
+
     enum CargoDocente{
         AY2,
         AY1,
@@ -10,8 +22,41 @@ public class SistemaSIU {
     }
 
     public SistemaSIU(InfoMateria[] infoMaterias, String[] libretasUniversitarias){
-        throw new UnsupportedOperationException("Método no implementado aún");	    
-    }
+        for (int i=0; i<infoMaterias.length; i++){
+
+            ListaEnlazada<String> listaAlumnos= new ListaEnlazada<>();
+            ListaEnlazada<Integer> listaDocentes = new ListaEnlazada<>();
+
+
+            Tupla<ListaEnlazada<String>,ListaEnlazada<Integer>> datosPorMateriavariable =  
+            new Tupla<ListaEnlazada<String>, ListaEnlazada<Integer>>(listaAlumnos, listaDocentes);
+
+            for (int j=0; j<infoMaterias[0].getParesCarreraMateria().length; j++){
+
+                
+
+                String nombreCarrera=infoMaterias[i].getParesCarreraMateria()[j].getCarrera();
+                String nombreMateria = infoMaterias[i].getParesCarreraMateria()[j].getNombreMateria();
+
+                if (sistema.buscar(nombreCarrera) == false){
+                    Trie<Tupla<ListaEnlazada<String>,ListaEnlazada<Integer>>> trieCarrera = new Trie<Tupla<ListaEnlazada<String>,ListaEnlazada<Integer>>>();
+                    sistema.insertar(nombreCarrera,trieCarrera);
+
+                }
+
+                Trie<Tupla<ListaEnlazada<String>,ListaEnlazada<Integer>>> carrera=sistema.obtener(nombreCarrera);
+
+
+                carrera.insertar(nombreMateria, datosPorMateriavariable);
+                
+
+            }
+
+        }
+
+
+    ;	    
+}
 
     public void inscribir(String estudiante, String carrera, String materia){
         throw new UnsupportedOperationException("Método no implementado aún");
--- Trabajo Practico 2_v2/estudiantes/src/main/java/aed/trie.java
+++ Trabajo Practico 2_v2/estudiantes/src/main/java/aed/trie.java
@@ -2,7 +2,7 @@ package aed;
 
 import java.util.*;//para poder usar arrayList
 
-public class trie<T> {
+public class Trie<T> {
     private Nodo raiz;
 
 
@@ -19,7 +19,7 @@ public class trie<T> {
     }
 
 
-    public trie(){
+    public Trie(){
         this.raiz = null;
     }
 
